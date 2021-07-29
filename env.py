d008f1db797e5b246e856ebc1ad5791bab48f8eb
cmd-source <conorcrosbie09@gmail.com>
2021-07-29 15:24:41 +0000

    starting the python mini project

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "uMhGHm>Qpg2U0stSGs|.F7[m-=?7r")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")

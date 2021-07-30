89cac37896ad37f6b31260e51bad530aaf637449
cmd-source <conorcrosbie09@gmail.com>
2021-07-30 08:22:03 +0000

    adding in tasks.html

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "uMhGHm>Qpg2U0stSGs|.F7[m-=?7r")
-os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@myfirstcluster.ahihw.mongodb.net/task_manager?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")

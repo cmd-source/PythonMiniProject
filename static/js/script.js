ee0f8a7dcb5d462eeba85c56ba52780c508b8802
cmd-source <conorcrosbie09@gmail.com>
2021-08-02 18:00:14 +0000

    adding task form fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge:"right"});
     $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+      format: "dd mm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: "Select"
+      }
+    });
   });
   
       
